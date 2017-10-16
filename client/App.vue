a51a4a843e62f2521bb717d3d86cfbc17eb5e986
Matej Janjic <matej.janjic@infinum.hr>
2017-10-16 22:33:26 +0200

    Implement sidenav, routes and toolbar

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,41 @@
 <template>
   <div :class="$style.main">
-    <router-view></router-view>
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">NMIBP web</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Menu</h3>
+        </div>
+      </md-toolbar>
+      <md-list>
+        <md-list-item>
+          <router-link exact :to="'/add'">
+            <md-icon>note_add</md-icon> <span>Add</span>
+          </router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link exact :to="'/search'">
+            <md-icon>search</md-icon> <span>Search</span>
+          </router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link exact :to="'/analisys'">
+            <md-icon>timeline</md-icon> <span>Analisys</span>
+          </router-link>
+        </md-list-item>
+      </md-list>
+
+    </md-sidenav>
+    <div :class="$style.content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -14,6 +49,12 @@
 
     data() {
       return {};
+    },
+
+    methods: {
+       toggleLeftSidenav() {
+        this.$refs.leftSidenav.toggle();
+      }
     }
   };
 </script>
@@ -21,6 +62,11 @@
 <style lang="scss" module>
   .main {
     min-height: 100vh;
-    background-color: '#FFFFFF';
+  }
+
+  .content {
+    max-width: 800px;
+    padding: 0 16px;
+    margin: 0 auto;
   }
 </style>
