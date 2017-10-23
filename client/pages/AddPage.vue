58ec599419f4e0d0cdc9894271cbd51b64209eff
Matej Janjic <matej.janjic@infinum.hr>
2017-10-24 00:09:35 +0200

    Refactor search action

--- client/pages/AddPage.vue
+++ client/pages/AddPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <movie-form
       v-model="movie"
       :status="movieFormStatus"
--- client/store/actions.js
+++ client/store/actions.js
@@ -10,15 +10,15 @@ export default {
       },
       body: JSON.stringify(search)
     }).then(resp => resp.json())
-    .then(({ query, rows }) => {
-      commit(types.SEARCH_SUCCESS, {
-        query,
-        rows
+      .then(({ query, rows }) => {
+        commit(types.SEARCH_SUCCESS, {
+          query,
+          rows
+        });
+        this.query = query;
+        this.queryResult = rows;
+      }).catch(() => {
+        commit(types.SEARCH_FAIL);
       });
-      this.query = query;
-      this.queryResult = rows;
-    }).catch(() => {
-      commit(types.SEARCH_FAIL);
-    });
   }
 };
