500e481fad1d28925ca7c0c427fdd829b5d69652
Matej Janjic <matej.janjic@infinum.hr>
2017-12-24 20:25:38 +0100

    Implement articles index and post comment controller

---
+++ exercise-2/server/controllers/articles.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { ObjectId } = require('mongodb');
+
+const router = express.Router();
+const db = require('../db');
+
+/**
+ *  GET articles page.
+ */
+router.get('/', async (req, res) => {
+  const articles = db.getCollection('articles');
+  res.render('articles', {
+    articles: await articles.find().limit(10).toArray()
+  });
+});
+
+
+router.post('/:articleId/comments', async (req, res) => {
+  const { articleId } = req.params;
+  const newComment = {
+    text: req.body.text,
+    time: new Date()
+  };
+
+  const articles = db.getCollection('articles');
+  await articles.findOneAndUpdate({
+    _id: ObjectId(articleId)
+  }, {
+    $push: { comments: newComment }
+  });
+
+  res.redirect(`/articles#${articleId}`);
+});
+
+
+module.exports = router;
---
+++ exercise-2/server/index.js
@@ -0,0 +1,73 @@
+const path = require('path');
+
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const sassMiddleware = require('node-sass-middleware');
+const errorHandler = require('errorhandler');
+const helmet = require('helmet');
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config();
+
+/**
+ * Controller (Route handlers)
+ */
+const articles = require('./controllers/articles');
+// const controllerName = require('./controllers/controllerName');
+
+/**
+ * Connect to database.
+ */
+require('./db').connect();
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'assets', 'styles'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Primary app routes.
+ */
+app.use('/articles', articles);
+// app.use('/route', controllerName);
+
+/**
+ * Error Handler for other errors.
+ */
+app.use(errorHandler());
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log(`${chalk.green('✓')} App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`); // eslint-disable-line no-console
+  console.log('  Press CTRL-C to stop\n'); // eslint-disable-line no-console
+});
+
+module.exports = app;
