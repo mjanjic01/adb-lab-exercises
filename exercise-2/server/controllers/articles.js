081304f9f6fd34fb138e41a03c82eb63eb50adc4
Matej Janjic <matej.janjic@infinum.hr>
2017-12-27 20:55:06 +0100

    Implement frequency and hot articles route

--- exercise-2/server/controllers/articles.js
+++ exercise-2/server/controllers/articles.js
@@ -1,3 +1,7 @@
+/* global emit */
+/* eslint-disable no-underscore-dangle, no-var, prefer-arrow-callback,
+                  func-names, prefer-destructuring, no-param-reassign */
+
 const express = require('express');
 const { ObjectId } = require('mongodb');
 
@@ -14,7 +18,76 @@ router.get('/', async (req, res) => {
   });
 });
 
+/**
+ *  GET most commented articles page.
+ */
+router.get('/hot', async (req, res) => {
+  const articles = db.getCollection('articles');
+  res.render('articles', {
+    articles: (await articles
+      .mapReduce(function () {
+        emit(this._id, this);
+      }, function (key, values) {
+        return values; // no-multiple
+      }, {
+        out: { inline: 1 },
+        sort: { numberOfComments: 1 }
+      })
+    ).map(row => row.value)
+  });
+});
 
+/**
+ *  GET most commented articles page.
+ */
+router.get('/frequency', async (req, res) => {
+  const articles = db.getCollection('articles');
+
+  // 🙈
+  const result = await articles
+    .mapReduce(function () {
+      emit(this.author, {
+        title: this.title,
+        description: this.description
+      });
+    }, function (key, values) {
+      if (values.title) {
+        return 0;
+      }
+      const pattern = /(?=\S*)([a-zA-Z0-9'-]+)/g;
+
+      const allWords = values.reduce(function (acc, val) {
+        return [acc, val.title, val.description].join(' ');
+      }, '');
+
+      const matchedWords = allWords.match(pattern);
+      const counts = matchedWords.reduce(function (stats, word) {
+        stats[word] = stats[word] ? stats[word] + 1 : 1;
+        return stats;
+      }, {});
+
+      const ordered = Object.keys(counts).reduce(function (acc, word) {
+        acc.push([word, counts[word]]);
+        return acc;
+      }, []).sort(function (a, b) {
+        return b[1] - a[1];
+      }).slice(0, 10);
+
+      const reduced = ordered.reduce(function (acc, count) {
+        acc[count[0]] = count[1];
+        return acc;
+      }, {});
+
+      return reduced;
+    }, { out: { inline: 1 } });
+  res.render('articles/frequency', {
+    frequencies: result
+  });
+});
+
+/**
+ * POST comment in article
+ */
 router.post('/:articleId/comments', async (req, res) => {
   const { articleId } = req.params;
   const newComment = {
