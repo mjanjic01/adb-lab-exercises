c6f90c8722c04130b9f2fdfa04ffa6a796b54964
Matej Janjic <matej.janjic@infinum.hr>
2018-01-02 18:04:37 +0100

    Fix mapreduce for word frequency

--- exercise-2/server/controllers/articles.js
+++ exercise-2/server/controllers/articles.js
@@ -43,40 +43,41 @@ router.get('/hot', async (req, res) => {
 router.get('/frequency', async (req, res) => {
   const articles = db.getCollection('articles');
 
-  // 🙈
   const result = await articles
-    .mapReduce(function () {
-      emit(this.author, {
-        title: this.title,
-        description: this.description
-      });
-    }, function (key, values) {
-      const pattern = /(?=\S*)([a-zA-Z0-9'-]+)/g;
+    .mapReduce(function () { // MAP
+      const wordPattern = /(?=\S*)([a-zA-Z0-9'-]+)/g;
+      const text = [this.title || '', this.description || ''].join(' ');
 
-      const allWords = (Array.isArray(values) ? values : [values]).reduce(function (acc, val) {
-        return [acc, val.title, val.description].join(' ');
-      }, '');
-
-      const matchedWords = allWords.match(pattern);
+      const matchedWords = text.match(wordPattern);
       const counts = matchedWords.reduce(function (stats, word) {
         stats[word] = stats[word] ? stats[word] + 1 : 1;
         return stats;
       }, {});
 
-      const ordered = Object.keys(counts).reduce(function (acc, word) {
-        acc.push([word, counts[word]]);
-        return acc;
-      }, []).sort(function (a, b) {
-        return b[1] - a[1];
-      }).slice(0, 10);
-
-      const reduced = ordered.reduce(function (acc, count) {
-        acc[count[0]] = count[1];
+      emit(this.author, counts);
+    }, function (key, values) { // REDUCE
+      return values.reduce(function (acc, counts) {
+        Object.keys(counts).forEach(function (word) {
+          acc[word] = (acc[word] || 0) + counts[word];
+        });
         return acc;
       }, {});
+    }, {
+      finalize: function (key, reducedValue) { // eslint-disable-line object-shorthand
+        const ordered = Object.keys(reducedValue).reduce(function (acc, word) {
+          acc.push([word, reducedValue[word]]);
+          return acc;
+        }, []).sort(function (a, b) {
+          return b[1] - a[1];
+        }).slice(0, 10);
 
-      return reduced;
-    }, { out: { inline: 1 } });
+        return ordered.reduce(function (acc, count) {
+          acc[count[0]] = count[1];
+          return acc;
+        }, {});
+      },
+      out: { inline: 1 },
+    });
   res.render('articles/frequency', {
     frequencies: result
   });
